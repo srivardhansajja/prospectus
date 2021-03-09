fe0ddbef276f4c3dbf6a3ff2f8d17bd3bd09a400
Braeden Smith <braedensmith2@gmail.com>
2021-03-09 15:30:41 -0600

    add initial DB connection

---
+++ backend/db.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql');
+// https://austinhale.medium.com/building-a-node-api-with-express-and-google-cloud-sql-9bda260b040f
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASS,
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error(`Error connecting: ${err.stack}`);
+    return;
+  }
+  console.log(`Connected to DB as thread id: ${connection.threadId}`);
+});
+
+module.exports = connection;
--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,17 @@
 /* eslint-disable no-console */
+require('dotenv').config();
 const express = require('express');
+const connection = require('./db');
 
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+app.get('/', async (req, res) => {
+  connection.query('SELECT * FROM `TEST`', (error, results, fields) => {
+    if (error) { throw error; }
+    res.json(results);
+    res.end();
+  });
 });
 
 app.get('/:param', (req, res) => {
