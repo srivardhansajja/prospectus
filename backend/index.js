0e99fa2fe75b3ea4cb2b8305f30b044a85d1b0ab
Srivardhan Sajja <sajja3@illinois.edu>
2021-04-17 01:43:06 -0500

    changed query structure in backend

--- backend/index.js
+++ backend/index.js
@@ -1,71 +1,130 @@
 /* eslint-disable no-console */
 
-require("dotenv").config();
-const express = require("express");
-const connection = require("./db");
-const cors = require("cors");
-const { response } = require("express");
+require('dotenv').config();
 
-const queries = require("./queries.js");
+const express = require('express');
+const cors = require('cors');
+const connection = require('./db');
+
+const queries = require('./queries.js');
 
 const app = express();
 const port = 3001;
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Home route reached!");
+// route for home
+app.get('/', (req, res) => {
+  res.send('Home route reached!');
 });
 
-app.get("/search", (req, res) => {
-  let sql = queries.searchQuery(
-    req.query.dept,
-    req.query.keywords,
-    req.query.userid
+// route for returning search results
+app.get('/search', (req, res) => {
+  const sql = queries.Search;
+  const { dept, keywords, userid } = req.query.dept;
+
+  connection.query(
+    sql,
+    [keywords, dept, keywords, dept, userid],
+    (err, data) => {
+      if (err) throw err;
+      res.json({
+        status: 200,
+        length: Object.keys(data).length,
+        data,
+        message: 'Search results returned successfully',
+      });
+    },
   );
-  console.log(req.query);
-  connection.query(sql, function (err, data, fields) {
+});
+
+// route for querying all courses in a user's wishlist
+app.get('/user/wishlist', (req, res) => {
+  const sql = queries.wishlistQuery;
+  const { userid, keywords } = req.query.userid;
+
+  connection.query(sql, [userid, keywords], (err, data) => {
     if (err) throw err;
     res.json({
       status: 200,
       length: Object.keys(data).length,
       data,
-      message: "Search results returned successfully",
+      message: "User's wishlist returned successfully",
     });
   });
 });
 
-app.get("/user/coursesTaken", (req, res) => {
-  let sql = queries.coursesTakenQuery(req.query.userid);
-  console.log(req.query);
-  connection.query(sql, function (err, data, fields) {
+// route for adding entry from wishlist
+app.post('/user/wishlist', (req, res) => {
+  const sql = queries.wishlistInsert;
+  const { userid, courseid } = req.body.userid;
+
+  connection.query(sql, [userid, courseid, courseid], (err, data) => {
+    if (err) {
+      res.json({
+        status: 400,
+        message: 'Duplicate',
+      });
+    } else {
+      res.json({
+        status: 200,
+        length: Object.keys(data).length,
+        data,
+        message: "Course added to user's wishlist successfully",
+      });
+    }
+  });
+});
+
+// route for updating entry in wishlist
+app.post('/user/wishlist/update', (req, res) => {
+  const sql = queries.wishlistUpdate;
+  const { userid, courseid, desc } = req.body.userid;
+
+  console.log(req.body);
+  connection.query(sql, [desc, courseid, userid], (err, data) => {
     if (err) throw err;
     res.json({
       status: 200,
       length: Object.keys(data).length,
       data,
-      message: "Courses taken returned successfully",
+      message: 'Course in wishlist updated successfully',
     });
   });
 });
 
-app.get("/user/wishlist", (req, res) => {
-  let sql = queries.wishlistQuery(req.query.userid);
-  console.log(req.query);
-  connection.query(sql, function (err, data, fields) {
+// route for deleting entry from wishlist
+app.delete('/user/wishlist', (req, res) => {
+  const sql = queries.wishlistDelete;
+  const { userid, courseid } = req.query.userid;
+
+  connection.query(sql, [userid, courseid], (err, data) => {
     if (err) throw err;
     res.json({
       status: 200,
       length: Object.keys(data).length,
       data,
-      message: "Courses taken returned successfully",
+      message: "Course deleted from user's wishlist successfully",
     });
   });
 });
 
-app.get("/:param", (req, res) => {
-  res.send(`Hello World, ${req.params.param}`);
+// route for querying all courses that a user has taken
+app.get('/user/coursesTaken', (req, res) => {
+  const sql = queries.coursesTakenQuery;
+  const { userid } = req.query.userid;
+
+  connection.query(sql, [userid], (err, data) => {
+    if (err) throw err;
+    res.json({
+      status: 200,
+      length: Object.keys(data).length,
+      data,
+      message: 'Courses taken returned successfully',
+    });
+  });
 });
 
 // app.get("/:param", (req, res) => {
--- backend/queries.js
+++ backend/queries.js
@@ -1,44 +1,81 @@
-// const queries = {
-//   searchQuery: search(dept, keywords, userid),
-// };
+/**
+ * MySQL queries for Prospectus
+ */
 
-function searchQuery(dept, keywords, userid) {
-  return (
-    search_1 +
-    "'" +
-    dept +
-    "'" +
-    search_2 +
-    "'" +
-    keywords +
-    "'" +
-    search_3 +
-    "'" +
-    userid +
-    "'" +
-    search_4
-  );
-}
+const Search = `
+  (
+    (
+      SELECT * FROM Prospectus.Courses 
+      WHERE CourseName LIKE CONCAT('%', ?, '%') AND CourseID LIKE ? 
+      ORDER BY CourseID asc
+    ) 
+    UNION 
+    (
+      SELECT * FROM Prospectus.Courses 
+      WHERE Description LIKE CONCAT('%', ?, '%') 
+      ORDER BY CourseID asc
+    ) 
+    LIMIT 10
+  ) 
+  UNION 
+  (
+    SELECT * FROM Prospectus.Courses pc 
+    WHERE pc.CourseID LIKE ? 
+    ORDER BY pc.CourseID asc 
+    LIMIT 10
+  ) 
+  UNION 
+  (
+    SELECT * FROM Prospectus.Courses c 
+    WHERE c.CourseID IN 
+    ( 
+      SELECT RelatedCourseID FROM Prospectus.CoursesRelationships cr 
+      WHERE cr.CourseID_cr IN ( 
+        SELECT CourseID_ct FROM Prospectus.CoursesTaken 
+        WHERE UserID_ct = ? 
+      ) AND cr.Relationship = 0
+    ) 
+    ORDER BY CourseID asc 
+    LIMIT 10
+  )
+`;
 
-function wishlistQuery(userid) {
-  return wishlist_1 + "'" + userid + "'";
-}
+const wishlistQuery = `
+  SELECT CourseID_wish, Description 
+  FROM Wishlist 
+  WHERE UserID_wish = ? AND Description LIKE CONCAT('%', ?, '%') 
+  ORDER BY CourseID_wish asc
+`;
 
-function coursesTakenQuery(userid) {
-  return coursesTaken_1 + "'" + userid + "'";
-}
+const wishlistInsert = `
+  INSERT INTO Wishlist(UserID_wish, CourseID_wish, Description)
+    SELECT ?, ?, CourseName
+    FROM Courses
+    WHERE CourseID = ?
+`;
 
-const search_1 = `(SELECT * FROM Prospectus.Courses pc WHERE pc.CourseID LIKE `; // @dept
-const search_2 = ` ORDER BY pc.CourseID asc LIMIT 10) UNION (SELECT * FROM Prospectus.Courses WHERE Description LIKE CONCAT('%', `; // @keywords
-const search_3 = `, '%') ORDER BY CourseID asc LIMIT 10) UNION (SELECT * FROM Prospectus.Courses c WHERE c.CourseID IN ( SELECT RelatedCourseID FROM Prospectus.CoursesRelationships cr WHERE cr.CourseID_cr IN ( SELECT CourseID_ct FROM Prospectus.CoursesTaken WHERE UserID_ct = `; // @userid
-const search_4 = ` ) AND cr.Relationship = 0) ORDER BY CourseID asc LIMIT 10);`;
+const wishlistDelete = `
+  DELETE FROM Wishlist 
+  WHERE UserID_wish = ? AND CourseID_wish = ?
+`;
 
-const coursesTaken_1 = `SELECT CourseID_ct FROM CoursesTaken WHERE UserID_ct = `; // @userid
+const wishlistUpdate = `
+  UPDATE Wishlist 
+  SET Description = ? 
+  WHERE CourseID_wish = ? AND UserID_wish = ?
+`;
 
-const wishlist_1 = `SELECT CourseID_wish FROM Wishlist WHERE UserID_wish = `; // @userid
+const coursesTakenQuery = `
+  SELECT CourseID_ct 
+  FROM CoursesTaken 
+  WHERE UserID_ct = ?
+`;
 
 module.exports = {
-  searchQuery,
+  Search,
   wishlistQuery,
+  wishlistInsert,
+  wishlistUpdate,
+  wishlistDelete,
   coursesTakenQuery,
 };
